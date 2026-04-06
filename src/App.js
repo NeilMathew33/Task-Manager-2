3c146a958adc03ae9fec62223b24092382299182
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-05 20:51:38 -0700

    New Taskbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddTask from './Components/AddTask';
+import ViewAllTasks from './Components/ViewAllTasks';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element= {<AddTask />} />
+      <Route path="/ViewAll" element= {<ViewAllTasks />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
