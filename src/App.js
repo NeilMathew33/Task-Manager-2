dfd447d08dbeecf8cddd7fe03ac94a8a706d1c51
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-22 19:25:54 -0700

    added signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddTask from './Components/AddTask';
 import ViewAllTasks from './Components/ViewAllTasks';
 import Login from './Components/Login';
+import SignUp from './Components/Signup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/Add" element= {<AddTask />} />
       <Route path="/ViewAll" element= {<ViewAllTasks />} />
       <Route path="/" element= {<Login />} />
+      <Route path="/SignUp" element= {<SignUp />} />
     </Routes>
     </BrowserRouter>
   );
